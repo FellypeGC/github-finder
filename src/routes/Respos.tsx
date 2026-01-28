4f0a6b36a40e8b2cf1b5fd1ebdbc3a2569da6c5e
FellypeGC <gabrielfellypesteve@gmail.com>
2026-01-28 19:32:49 -0300

    update: restating user repos

--- src/routes/Respos.tsx
+++ src/routes/Respos.tsx
@@ -1,10 +1,24 @@
+import type { RepoProps } from '../types/repo'
 import { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import BackBtn from '../components/BackBtn'
 import classes from "./Repos.module.css"
 
 const Respos = () => {
-  const { username } = useParams()
+  const { username } = useParams();
+  const [repos, setRepos] = useState<RepoProps | null>(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const loadRepos = async function (username: string) {
+      setIsLoading(true);
+      const res = await fetch(`https://api.github.com/users/${username}/repos`);
+      const data = await res.json();
+      setIsLoading(false);
+      console.log(data);
+    }
+    loadRepos(username);
+  }, [])
 
   return (
     <div>
