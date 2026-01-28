a36b3d0413254f35e6eda43f47eee4b1061c5af7
FellypeGC <gabrielfellypesteve@gmail.com>
2026-01-28 20:05:37 -0300

    update: css repos page

--- src/components/BackBtn.tsx
+++ src/components/BackBtn.tsx
@@ -1,3 +1,4 @@
+import classes from "./BackBtn.module.css"
 import { useNavigate } from 'react-router-dom'
 
 const BackBtn = () => {
@@ -5,7 +6,7 @@ const BackBtn = () => {
 
   return (
     <>
-      <button onClick={() => navigate(-1)}>Voltar</button>
+      <button className={classes.back_btn} onClick={() => navigate(-1)}>Voltar</button>
     </>
   )
 }
